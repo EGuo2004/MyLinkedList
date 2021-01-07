56bab150c7319bf74367637dec43af7dc60a7df6
unknown <>
2021-01-06 23:16:04 -0500

    Adds Constructor

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,3 +1,14 @@
-public class MyLinkedList {
+public class MyLinkedList{
+ private int size;
+ private Node start,end;
 
+ public MyLinkedList() {
+   size = 0;
+   start = null;
+   end = null;
+ }
+
+ public int size() {
+   return size;
+ }
 }
