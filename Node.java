a0255c0f18b770c225e6e1073cf747c1ce0f5de6
unknown <>
2021-01-06 23:10:18 -0500

    Adds get and set methods for instance variables

--- Node.java
+++ Node.java
@@ -5,4 +5,21 @@ public class Node {
   public Node(String value) {
     data = value;
   }
+
+  public void setNext(Node x) {
+    next = x;
+  }
+
+  public Node getNext() {
+    return next;
+  }
+
+  public void setPrev(Node x) {
+    prev = x;
+  }
+
+  public void getPrev() {
+    return prev;
+  }
+
 }
